053470f48bacf075d6a6544ed378ce7b85347a8e
Bana Budhian <budhiana.bana@gmail.com>
2023-08-07 16:51:51 +0700

    update

--- lib/config/app_route.dart
+++ lib/config/app_route.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../features/authentication/domain/entities/auth.dart';
-import '../features/authentication/presentation/cubit/auth_cubit.dart';
 import '../features/home/presentasion/pages/home_main.dart';
 import '../features/login/presentation/pages/login_page.dart';
 import '../features/splash/view/splash_page.dart';
 
 class AppRoute {
-  static GoRouter get routes {
+  static GoRouter getRoutes(AuthenticationStatus isAuth) {
     return GoRouter(
       initialLocation: '/splash',
       redirect: (BuildContext context, GoRouterState state) {
-        bool statusAkhir = false;
-        BlocListener<AuthCubit, AuthState>(listener: (context, state) {
-          print('masuk');
-          statusAkhir = state.status == AuthenticationStatus.unauthenticated ? true : false;
-        });
-        if (statusAkhir) {
+        if (isAuth == AuthenticationStatus.unauthenticated) {
           return '/login';
+        } else if (isAuth == AuthenticationStatus.authenticated) {
+          return '/';
         } else {
-          print(statusAkhir);
           return null;
         }
       },
